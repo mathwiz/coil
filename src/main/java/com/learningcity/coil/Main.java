e1b3ad6af9a0321617d46b233580e5a419d8daf6
Yohan Lee <yohanlee@yahoo.com>
2013-06-27 21:01:02 -0400

    Main class

---
+++ src/main/java/com/learningcity/coil/Main.java
@@ -0,0 +1,14 @@
+package com.learningcity.coil;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yohan
+ * Date: 6/22/13
+ * Time: 6:55 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Started");
+    }
+}
