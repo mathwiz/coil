8b3a67c02c38be42a4e9ba6288b164cdce5a4db7
Yohan Lee <yohanlee@yahoo.com>
2013-07-06 18:56:44 -0400

    new

---
+++ src/main/java/com/learningcity/coil/model/Widget.java
@@ -0,0 +1,20 @@
+package com.learningcity.coil.model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yohan
+ * Date: 7/6/13
+ * Time: 6:48 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Widget {
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
