5596982d8ea8792121908bfb4ce2451bd5f1866b
Yohan Lee <yohanlee@yahoo.com>
2013-07-06 19:06:39 -0400

    test

---
+++ src/test/java/com/learningcity/coil/MainTest.groovy
@@ -0,0 +1,14 @@
+package com.learningcity.coil
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yohan
+ * Date: 7/6/13
+ * Time: 7:02 PM
+ * To change this template use File | Settings | File Templates.
+ */
+class MainTest {
+    void testMain() {
+        Main.main("hi");
+    }
+}
