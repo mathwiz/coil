05cf7b3e1e30196d64fb3f79309aebfc1d44d7b9
Yohan Lee <yohanlee@yahoo.com>
2013-07-17 22:12:28 -0400

    new

---
+++ src/main/java/com/learningcity/coil/view/Main.java
@@ -0,0 +1,14 @@
+package com.learningcity.coil.view;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Yohan
+ * Date: 7/13/13
+ * Time: 7:33 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Need to implement");
+    }
+}
