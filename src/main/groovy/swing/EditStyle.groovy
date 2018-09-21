5f944d04e88276fb21bb81d9e4d5cadd3006af42
mathwiz <mathwiz@gmail.com>
2018-09-21 08:50:45 -0400

    delete?

--- src/main/groovy/swing/EditStyle.groovy
+++
@@ -1,9 +0,0 @@
-package swing
-
-class EditStyle {
-    def size, color, font
-
-    def propertyString() {
-        "Size: ${size}, Color: ${color}, Font: ${font}"
-    }
-}
--- src/main/groovy/swing/Main.groovy
+++
@@ -1,8 +0,0 @@
-package swing
-
-class Main {
-    static void main(String[] args) {
-        def es = new EditStyle(size: 12, color: "Blue", font: "Times")
-        println es.propertyString()
-    }
-}
