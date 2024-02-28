1aa015d77e30ed8379455dd4967db8a43259f530
Yohan Lee <yohanlee@yahoo.com>
2024-02-28 15:13:34 -0500

    coil for bio

--- src/main/java/coil/Hello.java
+++ src/main/java/coil/Hello.java
@@ -1,5 +1,10 @@
+package coil;
+
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        for(String s : args) {
+            System.out.println(s);
+        }
     }
 }
